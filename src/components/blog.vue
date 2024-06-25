2291564a70fde5cb22deb1ff66c0fbbf395cfa86
Your Name <you@example.com>
2024-06-25 18:52:18 +0100

    nouvel ajout

--- src/components/blog.vue
+++ src/components/blog.vue
@@ -3,7 +3,7 @@
     <div>
       All Posts
     </div>
-    <div>
+    <div >
       Most Reader
     </div>
     <div>
@@ -20,24 +20,54 @@
       <div @click="toggleSearch"><ion-icon name="close-outline"></ion-icon></div>
     </div>
   </div>
-
   </form>
+  <section class="unified-container">
+  <img :src="article1" alt="article1"  width="50%" height="100%" /> 
+  <article class="content">
+    <div class="info">
+    <div class="profile">
+    <img :src="profile1" alt="profile"/>
+    </div>
+    <div class="info1">
+    <p >{{ username }}</p><span id="dots" @click="share"><ion-icon name="ellipsis-vertical"></ion-icon></span>
+    <p >{{ currentdate }} . {{currenttime}}</p> 
+    </div>
+    </div>
+    <div class="share" v-show="shar">
+      <ion-icon name="arrow-redo-outline"></ion-icon>
+    <input type="submit" value="Share Post">
+    </div>
+  </article>
+  </section>
 </template>
 
 <script>
+import article1 from '@/assets/article1.webp';
+import profile1 from '@/assets/profile1.jpg'
 export default {
   data() {
+    const now = new Date();
+    const currentdate= now.toLocaleDateString();
+    const currenttime= now.toLocaleTimeString();
     return {
-        oui:false
+        oui:false,
+        shar:false,
+        article1,
+        username:"khalid kidarine",
+        profile1,
+        currentdate,
+        currenttime
     }
   },
    methods: {
     toggleSearch() {
       this.oui = !this.oui;
+    },
+    share(){
+       this.shar= !this.shar;
     }
   }
   }
-
 </script>
 
 <style scoped>
@@ -49,7 +79,7 @@ form {
   height: 50px;
   padding: 5px;
   align-items: center;
-  margin-left: 20%;
+  margin-left: 10%;
 }
 div {
   display: block;
@@ -65,7 +95,64 @@ div:hover {
     flex-direction: row;
 }
 input[type=text]{
-
+    outline: none;
+    border: none;
     border-bottom: 1px solid gray;
+}
+.unified-container{
+  border: 1px solid gray;
+  display: flex;
+  width: 70%;
+  height: 500px;
+  margin-left: 15%;
+  padding: 0%;
+  cursor: pointer;
+}
+.article{
+  float: right;
+}
+.profile{
+  border: 5px solid #fff;
+  border-radius:50% ;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  width: 50px;
+}
+.profile img{
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+.content{
+  width: 50%;
+}
+.info{
+  display: flex;
+  flex-direction: row;
+  width: 97%;
+  height: 65px;
+}
+.info1{
+  width: 70%;
+  height: 80%;
+  margin-left:5px ;
+  margin-top: -7px;
+  padding: 0%;
+}
+#dots{
+  float: right;
+  margin-top:-10px ;
+}
+.share{
+  width: 60%;
+  height: 40px;
+  background-color: aqua;
+  margin-left:100px ;
+  border: 2px solid white;
+  box-shadow: 2px 2px 10px black;
+  padding-top:6px ;
 }
 </style>
